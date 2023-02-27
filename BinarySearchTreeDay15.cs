de096fe1f42aad8d04a1023aa36c04229219a10f
poojakhandale <poojakhandale2000@gmail.com>
2023-02-27 22:58:29 +0530

    Binary Search Tree Logic

---
+++ BinarySearchTreeDay15.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTreeDay15
+    {
+         
+        public Node root;
+
+        public BinarySearchTreeDay15()
+        {
+            root = null;
+        }
+
+        public void Insert(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+
+                while (true)
+                {
+                    parent = current;
+
+                    if (data < current.data)
+                    {
+                        current = current.left;
+
+                        if (current == null)
+                        {
+                            parent.left = newNode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+
+                        if (current == null)
+                        {
+                            parent.right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void InorderTraversal(Node node)
+        {
+            if (node != null)
+            {
+                InorderTraversal(node.left);
+                Console.Write(node.data + " ");
+                InorderTraversal(node.right);
+            }
+        }
+    }
+}
