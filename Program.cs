813682d7b163146f527b8cf6a08e7cb34a1b1076
poojakhandale <poojakhandale2000@gmail.com>
2023-02-27 23:10:58 +0530

    Added the size method to check the nodes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BinarySearchTree;
+
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Binary search programs");
+        {
+          BinarySearchTreeDay15 bst = new BinarySearchTreeDay15();
+            bst.Insert(56);
+            bst.Insert(30);
+            bst.Insert(70);
+
+            Console.WriteLine("Inorder Traversal of BST:");
+            bst.InorderTraversal(bst.root);
+        }
     }
 }
+
